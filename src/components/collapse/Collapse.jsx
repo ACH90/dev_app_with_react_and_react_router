c3e621606afa998cdd4f39072418fcd45b1e0bd3
ACH90 <chassagne.alexandre@gmail.com>
2025-04-15 15:45:53 +0200

    Add collapses

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,1 +1,21 @@
- 
+import { useState } from "react";
+import styles from "./Collapse.module.scss"; // optionnel si tu veux le styliser
+
+const Collapse = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles.collapse}>
+      <button
+        className={styles.collapseHeader}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <h2>{title}</h2>
+        <span>{isOpen ? "▲" : "▼"}</span>
+      </button>
+      {isOpen && <div className={styles.collapseContent}>{content}</div>}
+    </div>
+  );
+};
+
+export default Collapse;
--- src/pages/APropos/APropos.jsx
+++ src/pages/APropos/APropos.jsx
@@ -1,11 +1,21 @@
 import styles from "./Aporpos.module.scss";
+import Collapse from "/src/components/collapse/Collapse";
 
 const APropos = () => {
   return (
     <div className={styles.apropos}>
       <div className={styles.banner}></div>
       <div>
-        <section className={styles.section}></section>
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées garantissent une fiabilité totale"
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa"
+        />
+        <Collapse title="Services" content="La qualité du service..." />
+        <Collapse title="Sécurité" content="La sécurité..." />
       </div>
     </div>
   );
