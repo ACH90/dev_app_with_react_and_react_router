acc4d72570f9fec771ca22ba725f02d8722e60b8
ACH90 <chassagne.alexandre@gmail.com>
2025-04-17 18:22:12 +0200

    Install gh-pages

--- src/components/router/router.jsx
+++ src/components/router/router.jsx
@@ -1,32 +1,36 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 // Pages
 import Home from "/src/pages/Home/Home.jsx";
 import NotFoundPage from "/src/pages/NotFoundPage/NotFoundPage.jsx";
 import APropos from "/src/pages/APropos/APropos.jsx";
 import Logement from "/src/pages/Logement/Logement.jsx";
-// Header & Footer
+// Layout
 import MainLayout from "/src/layouts/MainLayout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to="/Accueil" replace />,
+  },
+  {
+    path: "/Accueil",
     element: <MainLayout />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <Home />,
+      },
+      {
+        path: "apropos",
+        element: <APropos />,
+      },
+      {
+        path: "logement/:id",
+        element: <Logement />,
       },
       {
         path: "*",
         element: <NotFoundPage />,
-      },
-      {
-        path: "/apropos",
-        element: <APropos />,
-      },
-      {
-        path: "/logement/:id",
-        element: <Logement />,
       },
     ],
   },
